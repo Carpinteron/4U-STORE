7bc1f2162fcdef9a644cfa2826409072ed186799
pau <pzarante@uninorte.edu.co>
2023-10-01 23:17:06 -0500

    vamos mis amores

--- src/PrincipalAD.java
+++ src/PrincipalAD.java
@@ -1939,11 +1939,13 @@ public class PrincipalAD extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        
         AñadirUsuarios("Usuarios","CedulasAdmins");
-          RegistroPanel.setVisible(false);
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        RegistroPanel.setVisible(true);
+        Registrar registro = new Registrar(user,cedula);
+        registro.setVisible(true);
+     
     }//GEN-LAST:event_jButton3ActionPerformed
     public void AñadirUsuarios(String file_name, String file_ced) {
         try {
